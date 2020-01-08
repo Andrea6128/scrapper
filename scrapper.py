6b82cc2cb8d2304066e8959445ae6e429464900c
Andrea Petriku <iam.andreapetriku@gmail.com>
2020-01-08 14:10:39 +0100

    Web scrapper test program, trying things.

--- scrapper.py
+++ scrapper.py
@@ -1,14 +1,30 @@
 import requests
 from bs4 import BeautifulSoup
+import re
 
-URL = 'https://hodinky.heureka.cz/casio-a700weg-9a/#'
+URL = 'https://hodinky.heureka.cz/casio-a700weg-9a/'
 
 headers = {
     "User-Agent": "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/79.0.3945.117 Safari/537.36"
 }
 
-stranka = requests.get(URL, header=header)
+stranka = requests.get(URL, headers=headers)
 
 soup = BeautifulSoup(stranka.content, 'html.parser')
 
-print(soup.prettify())
+# print(soup.prettify())
+
+# def count_records(search_word):
+#     scrapped_text = soup.get_text()
+#     scrapped_lower = scrapped_text.lower()
+#
+#     word_orig = search_word
+#     word_low = word_orig.lower()
+#
+#     print("The word", word_low, "is present", scrapped_lower.count(word_low), "times in the file.")
+#
+# count_records("CASIO")
+# print(soup)
+
+a = soup.findAll(text=re.compile("Kč"))
+print(a)
