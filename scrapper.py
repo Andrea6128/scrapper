cea8c8dfc59ace602f566a1f822e7aec0e79a8e7
Andrea Petriku <iam.andreapetriku@gmail.com>
2020-01-10 11:00:58 +0100

    First working version.

--- scrapper.py
+++ scrapper.py
@@ -3,16 +3,28 @@ from bs4 import BeautifulSoup
 import re
 import sys
 import string
+import json
 
-print("Heureka.cz Price Scrapper - (c) 2020 Andrea Petriku")
+print("Heureka.cz Price Scrapper v0.5 - (c) 2020 Andrea Petriku")
 print("Usage: python scrapper.py <http[s]://<product>.heureka.cz/product-name-as-found-by-google>")
 print()
 
 # saves shell argument into URL var
 URL = sys.argv[1]
 
-if "http" not in URL or "https" not in URL:
+if "http" in URL or "https" in URL or "-h" in URL:
+    pass
+
+else:
     print("Please enter a valid URL (example: https://website.dk/)")
+    sys.exit()
+
+if "-h" in URL or "--help" in URL:
+    print("This program scraps given Heureka.cz product URL (copy and paste it manually into your terminal from your browser)")
+    print("and tries to find the lowest price available and name of the corresponding eshop.\n")
+    print("Syntax: python scrapper.py <http[s]://<product>.heureka.cz/product-name-as-found-by-google>")
+    print("Example: python scrapper.py https://notebooky.heureka.cz/lenovo-thinkpad-x1-yoga-4-20qf0025mc/#")
+
     sys.exit()
 
 headers = {
@@ -21,60 +33,30 @@ headers = {
 
 stranka = requests.get(URL, headers=headers)
 soup = BeautifulSoup(stranka.content, 'html.parser')
-vycuc = soup.findAll(text=re.compile("Kč"))
+# vycuc = soup.findAll(text=re.compile("Kč"))
+nibble = soup.find(text=re.compile("__advert_product_info = {")) # find specified string
+nibble_stripped1 = nibble.strip("var __advert_product_info = ") # cut this string out
+nibble_stripped2 = nibble_stripped1.strip(";") # cut ';'
 
-# remove unwanted chars and print product_name
-product_name = re.sub(r" od .*$", "", vycuc[0])
+base_dict = json.loads(nibble_stripped2) # convert string to a dict
+
+product_name = base_dict['name']
 print("Product name:", product_name)
 
-# prices start at 3th item in list and end at -1st place
-prices = vycuc[3:-1]
+shops = base_dict['shops']
 
-# print(vycuc)
+prices_list = []
+for index_number in range(len(shops)):
+    name_price_line = shops[str(index_number)]
+    prices_list.append(name_price_line['price'])
 
-new_list = []
-for i in range(len(prices)):
-    value = prices[i].strip(' Kč')
-    new_list.append(value)
+prices_list.sort()
+lowest_price = prices_list[0]
 
-# remove xx\xa0 records
-for unwanted in new_list:
-    if "\xa0" in unwanted:
-        new_list.remove(unwanted)
+for index_number in range(len(shops)):
+    name_price_line = shops[str(index_number)]
+    if name_price_line['price'] == lowest_price:
+        shop_name = name_price_line['name']
 
-# remove spaces
-new_list2 = []
-for i in range(len(new_list)):
-    a = re.sub(r"\s+", "", new_list[i])
-    new_list2.append(a)
-
-print(new_list2)
-
-# remove anything other than prices
-rec_index = 0
-try:
-    for record in new_list2:
-        for letter in record:
-            for char in range(10):
-                end_of_record = 0
-                print("letter:", letter, "char:", char)
-                if letter == str(char):
-                    print("letter:", letter, "char:", char)
-                    print("letter == char .. ok")
-                    break
-                else:
-                    break
-
-            print("end of record")
-            end_or_record = 1
-            print("popping: ", new_list2[rec_index])
-            new_list2.pop(rec_index)
-
-    rec_index += 1
-except IndexError:
-    pass
-
-print(new_list2)
-
-new_list2.sort()
-print("The lowest price is:", new_list2[0], "CZK")
+print("The lowest price:", lowest_price, "CZK")
+print("Shop name:", shop_name)
